3d61f8e56a7c7a38fd749bc59cac93a1508bf580
josimar-jr <shikamarujunior@gmail.com>
2017-04-13 13:32:25 -0300

    Ajuste do nome da classe de teste da classe tipoCurva

---
+++ src/tests/Test_tipoCurva.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import wfmpack.tipoCurva;
+
+public class Test_tipoCurva {
+
+	@Test
+	public void testTipoCurva() {
+		int valorSemana = 0;
+		int valorDomingo = 7;
+		int valorSexta = 5;
+		
+		String strSemana = "SEM";
+		String strDomingo = "DOM";
+		String strSexta = "SEX";
+		
+		tipoCurva tpCurvaSemana = tipoCurva.SEM;
+		tipoCurva tpCurvaDomingo = tipoCurva.DOM;
+		tipoCurva tpCurvaSexta = tipoCurva.SEX;
+		
+		assertEquals( tpCurvaSemana.getValor(), valorSemana );
+		assertEquals( tpCurvaDomingo.getValor(), valorDomingo );
+		assertEquals( tpCurvaSexta.getValor(), valorSexta );
+		
+		assertEquals( tipoCurva.getStringPeloTipo(tpCurvaSemana), strSemana );
+		assertEquals( tipoCurva.getStringPeloTipo(tpCurvaDomingo), strDomingo );
+		assertEquals( tipoCurva.getStringPeloTipo(tpCurvaSexta), strSexta );
+		
+	}
+
+}
